cf64684b09a791a308842f4c9e7a895c248905c1
Yinon Cohen <yinonc@wix.com>
2018-03-28 14:59:02 +0300

    updated server

--- Server/Server.js
+++ Server/Server.js
@@ -9,6 +9,7 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const _ = require('lodash');
 
 let PORT = 3300;
 let address = "127.0.0.1";
@@ -25,43 +26,45 @@ process.argv.forEach(function (val, index, array) {
 let server = http.createServer(function (req, res) {
     console.log(`${req.method} request for ${req.url}`);
     console.log(req.connection.remoteAddress);
+    let fileName = req.url;
+    if (fileName === '/' || fileName === '/index.html' || fileName === 'index.html') {
+        fileName = 'index.html';
+    }
+    if (isFileExistsInDirectory('.', fileName)) {
+        if (_.includes(fileName, '.html')) {
+            fs.readFile(`./${fileName}`, function (err, data) {
+                if (err) {
+                    throw err;
+                }
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data);
+            });
+        }
+        else if (req.url.match(/.jpg$/)) {
+            let imgPath = path.join(__dirname, 'images', fileName);
+            let imgStream = fs.createReadStream(imgPath);
 
-    if (req.url === '/' || req.url === '/index.html' || req.url === 'index.html') {
-        fs.readFile('./index.html', function(err, data) {
+            res.writeHead(200, {'Content-Type': 'image/jpeg'});
+
+            imgStream.pipe(res);
+        }
+
+        else if (req.url.match(/.png$/)) {
+            let imgPath = path.join(__dirname, 'images', fileName);
+            let imgStream = fs.createReadStream(imgPath);
+
+            res.writeHead(200, {'Content-Type': 'image/png'});
+
+            imgStream.pipe(res);
+        }
+    } else {
+        fs.writeFileSync(fileName, 'File Not Found. Please check your request.\n', function (err) {
             if (err) {
                 throw err;
             }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(data);
         });
-    }
-
-    else if (req.url.match(/.jpg$/)) {
-        let imgPath = path.join(__dirname, 'images', req.url);
-        let imgStream = fs.createReadStream(imgPath);
-
-        res.writeHead(200, {'Content-Type': 'image/jpeg'});
-
-        imgStream.pipe(res);
-    }
-
-    else if (req.url.match(/.png$/)) {
-        let imgPath = path.join(__dirname, 'images', req.url);
-        let imgStream = fs.createReadStream(imgPath);
-
-        res.writeHead(200, {'Content-Type': 'image/png'});
-
-        imgStream.pipe(res);
-    }
-
-    else {
-        fs.readFile('./notFound', function(err, data) {
-            if (err) {
-                throw err;
-            }
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(data);
-        });
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(data);
     }
 });
 
@@ -73,3 +76,12 @@ require('dns').lookup(require('os').hostname(), function (err, add, fam) {
     server.listen(PORT, address);
     console.log(`Server is running on ip ${address}, port ${PORT}.`);
 });
+
+function isFileExistsInDirectory(dirPath, fileName) {
+    let files = fs.readdirSync(dirPath, function(err) {
+        if (err) {
+            throw err;
+        }
+    });
+    return _.includes(files, fileName);
+}
