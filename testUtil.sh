cac2799f8922de21c3be201ac8c67274faf3ec83
Yinon Cohen <yinonc@wix.com>
2018-04-20 16:32:12 +0300

    integration tests working

--- testUtil.sh
+++ testUtil.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
+# running the server and outputting it's log to temporary file
 output=$(mktemp "${TMPDIR:-/tmp/}$(basename 0).XXX")
 node server/Server &> $output &
 server_pid=$!
 echo "Initializing the server with pid: $server_pid . Wait:\n"
+
+# waiting until the server is listening
 until grep -i 'Server is listening on ip ' $output
 do
   sleep 1
 done
-npm run test
+
+npm run jasmine
 code=$?
+
+# finished running tests, time to kill the server
 kill -9 $server_pid
 echo "Server killed"
+
+# exiting with the test's exit code
 exit $code
